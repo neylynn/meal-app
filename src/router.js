7822d5c2a5133d3ec63d18dca67c4057ee37d9a0
neylynn <neylynn97@gmail.com>
2023-09-18 19:20:55 +0630

    first commit

---
+++ src/router.js
@@ -0,0 +1,57 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import store from '@/store.js';
+
+Vue.use(Router);
+
+const router = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('./views/About.vue'),
+            meta: {
+                authRequired: true
+            }
+        },
+        {
+            path: '/menu',
+            name: 'menu',
+            component: () => import('./views/Menu.vue')
+        },
+        {
+            path: '/sign-in',
+            name: 'signin',
+            component: () => import('./views/Signin.vue')
+        },
+        {
+            path: '/join',
+            name: 'join',
+            component: () => import('./views/Join.vue')
+        }
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.authRequired)) {
+        if (!store.state.isAuthenticated) {
+            next({
+                path: '/sign-in'
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
+
+export default router;
